f2afa3f3e8af97bd3e69bca7ef1542c35c5c7093
kunal1619 <102772307+kunal1619@users.noreply.github.com>
2023-01-04 15:19:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Store from './Component/Store'
+import Header from './Component/Header'
+
+
+const App = () => {
+
+  return (
+    <div>
+    <Header/>
+    <Store/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+// Dog Api : https://dog.ceo/api/breeds/image/random
+// Cat fact : https://catfact.ninja/fact
